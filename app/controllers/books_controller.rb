d05c98d3413edd3ddbe658432e59148cc3110431
John Gambino <jgambin1@uncc.edu>
2020-04-14 14:22:48 +0000

    Added a back button and Recent Searches page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,24 @@
 class BooksController < ApplicationController
+    def index
+        @book = Book.all
+    end
+    
+    
     def show 
         @book = Book.find(params[:id])
     end 
     
     def new 
+        @book = Book.new
     end 
     
     def create 
         @book = Book.new(book_params)
-        @book.save 
-        
-        redirect_to @book
-
+        if @book.save 
+            redirect_to @book
+        else
+            render 'new'
+        end
     end 
     
 end
