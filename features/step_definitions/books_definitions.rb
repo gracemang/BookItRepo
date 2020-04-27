1e5215e568f129863f28bd6a2067b196fbdea645
EC2 Default User <ec2-user@ip-172-31-93-248.ec2.internal>
2020-04-27 22:28:35 +0000

    Test cases are complete

--- features/step_definitions/books_definitions.rb
+++ features/step_definitions/books_definitions.rb
@@ -1,82 +1,37 @@
-Given("I am on the homepage") do
-  #pending # Write code here that turns the phrase above into concrete actions
-  visit root_path
-end
-
-Given("I am on the {string} page") do |string|
-  #pending # Write code here that turns the phrase above into concrete actions
-  expect(page).to have_content(string)
-end
-
-Then("I type an ISBN in the {string} box") do |string|
-  #pending # Write code here that turns the phrase above into concrete actions
-  fill_in('Enter ISBN Number', :with => '9780132126953')
-end
-
-When("I click on the {string} box") do |string|
-  #pending # Write code here that turns the phrase above into concrete actions
-  click_button string
-end
-
-Then("I should be on the {string} page") do |string|
-  #pending # Write code here that turns the phrase above into concrete actions
-  expect(page).to have_content(string)
+ Given("I am on the homepage") do
+   visit root_path
 end
 
 When("I click on the {string} link") do |string|
-  #pending # Write code here that turns the phrase above into concrete actions
   click_link string
 end
 
-Then("I should see the {string} field") do |string|
-  #pending # Write code here that turns the phrase above into concrete actions
-  expect(page).to have_content(string)
-end
-
-Then("I should be {string} page") do |string|
-  #pending # Write code here that turns the phrase above into concrete actions
-  expect(page).to have_content(string)
-end
-
-When("I click on the {string} field") do |string|
-  #pending # Write code here that turns the phrase above into concrete actions
+Then("I should be on the {string} page") do |string|
   expect(page).to have_content(string)
 end
 
 Then("I type my name in the {string} field") do |string|
-  #pending # Write code here that turns the phrase above into concrete actions
-  fill_in('Name', :with => 'John Gambino')
+  fill_in('Name', :with => 'Bob Brown')
 end
 
 Then("I type in my email in the {string} field") do |string|
-  #pending # Write code here that turns the phrase above into concrete actions
-  fill_in('Email', :with => 'jgambin1@uncc.edu')
+  fill_in('Email', :with => 'Bob12345@gmail.com')
 end
 
 Then("I type in my password in the {string} field") do |string|
-  #pending # Write code here that turns the phrase above into concrete actions
-  fill_in('Password', :with => '123456')
+  fill_in('Password', :with => 'Yellow')
 end
 
 Then("I type in my password again in the {string} field") do |string|
-  #pending # Write code here that turns the phrase above into concrete actions
-  fill_in('Password confirmation', :with => '123456')
-end
-
-When("I click on the {string} box") do |string|
-  #pending # Write code here that turns the phrase above into concrete actions
-  #find(string).trigger("click")
-  click_button string
+  fill_in('Password confirmation', :with => 'Yellow')
 end
 
 When("I click on the {string} button") do |string|
- # pending # Write code here that turns the phrase above into concrete actions
   click_button string
 end
 
-Then("I should be on {string} page") do |string|
-  #pending # Write code here that turns the phrase above into concrete actions
-  expect(page).to have_content(string)
+Then("I should be on homepage") do
+  visit root_path 
 end
 
 Then("I type in the number on the {string} field") do |string|
